d45783d024ebd673b7cfd02fcbabf322e9c17081
seelenamt <dandrewe@gmail.com>
2024-01-19 07:59:13 +0000

    Create shutdown.py
    This might get embarrassing.

---
+++ scripts/shutdown.py
@@ -0,0 +1,5 @@
+# Calls pwangotchi's built in shutdown function. I don't know what I'm doing, so this might take a few attempts...
+
+from pwnagotchi.plugins import shutdown
+
+pwnagotchi.shutdown()
